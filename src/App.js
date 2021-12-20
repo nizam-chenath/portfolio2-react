2713d5eb5eea126fd90b085e36488cf3e857553f
nizam-chenath <nizamstudymajliz@gmail.com>
2021-12-20 11:06:37 +0530

    Home Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css"
 import Profile from "./container/Home/Profile";
-
+import Footer from "./container/Home/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Profile/>
+      <Footer/>
      </div>
   );
 }
---
+++ src/container/Home/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css'
+
+export default function footer() {
+    return (
+        <div className="footer-container">
+            <div className="footer-parent">
+                <img src={require ('../../../Assets/Home/footer.png').default}/>
+            </div>
+        </div>
+    )
+}
